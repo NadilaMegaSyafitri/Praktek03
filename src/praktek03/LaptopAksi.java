9de7e5ed2a4e3d61a34c667abc18332555b0c015
NadilaMegaSyafitri <32352445+NadilaMegaSyafitri@users.noreply.github.com>
2018-04-10 10:15:50 -0700

    menggunakan method set

--- src/praktek03/LaptopAksi.java
+++ src/praktek03/LaptopAksi.java
@@ -2,10 +2,11 @@ package praktek03;
 public class LaptopAksi {
     public static void main(String[] args) {
         Laptop s= new Laptop();
-        s.nama="Laptop";
-        s.merk="Asus";
-        s.warna="Putih";
-        s.harga=5000000;
+        
+        s.setNama("Laptop");
+        s.setMerk("Asus");
+        s.setWarna("Putih");
+        s.setHarga(5000000);
         
         s.cetakInfo();
     }
