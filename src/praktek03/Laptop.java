69ec7798139cf66f6d77a87e1c8910859fdc8585
NadilaMegaSyafitri <32352445+NadilaMegaSyafitri@users.noreply.github.com>
2018-04-10 10:11:23 -0700

    buat Getter and Setter

--- src/praktek03/Laptop.java
+++ src/praktek03/Laptop.java
@@ -9,4 +9,38 @@ void cetakInfo(){
         System.out.println("Nama \t:"+nama+"\n"+"Merk \t :" +merk+"\n"+"Warna \t"
                 + " :" +warna+"\n"+"harga \t :" +harga);
 }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getMerk() {
+        return merk;
+    }
+
+    public void setMerk(String merk) {
+        this.merk = merk;
+    }
+
+    public String getWarna() {
+        return warna;
+    }
+
+    public void setWarna(String warna) {
+        this.warna = warna;
+    }
+
+    public double getHarga() {
+        return harga;
+    }
+
+    public void setHarga(double harga) {
+        this.harga = harga;
+    }
+
+
 }
